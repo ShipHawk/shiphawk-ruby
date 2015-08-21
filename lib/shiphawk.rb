5cb89925c40fc540358b9cfe4810745262c4114d
Vincent Wilson <vincent.movieman@gmail.com>
2015-08-21 13:25:36 -0400

    Added the ability for the shiphawk client class to be used as a singleton

--- lib/shiphawk.rb
+++ lib/shiphawk.rb
@@ -13,9 +13,21 @@ module Shiphawk
     # ```ruby
     # client = ShiphawkClient::Client.new
     # ```
+    #
+    # You can also chose to use the class as a singleton
+    #
+    # ```ruby
+    # Shiphawk.{method_name}
+    # ```
+
     def configure
       yield self
       true
+    end
+
+    def method_missing(method, *args, &block)
+      return super unless self::Client.new.respond_to?(method)
+      self::Client.new.send(method, *args, &block)
     end
 
   end
