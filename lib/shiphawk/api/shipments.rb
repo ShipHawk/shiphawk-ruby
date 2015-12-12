0ed5363a8bcc3df2ef727b1a420e29a10785c5b6
Alex Hawkins <alex.hawkins@shiphaw.com>
2015-12-11 18:17:21 -0800

    Add Shipment Class to Api Module

--- lib/shiphawk/api/shipments.rb
+++ lib/shiphawk/api/shipments.rb
@@ -9,32 +9,128 @@ module ShipHawk
     #
     module Shipments
 
-      def shipments_index
-        collection_request shipments_path, 500
+      def self.create_shipment(params={})
+        puts params
+        response, api_key = ShipHawk.request(:post, "/shipments", api_key, params.to_json)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
-      def shipments_show shipment_id
-        entity_request_with_id shipments_path, shipment_id
+      # update/edit a particular Shipment
+      def self.update(shipment_id, params={})
+        response, api_key = ShipHawk.request(:put, "/shipments/#{shipment_id}", api_key, params)
+        puts response
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
-      def shipments_bol shipment_id
-        get_request shipments_path("#{shipment_id}/bol"), {}
+      # update ITN (Internal Transaction Number) for an international shipment
+      # @params [ itn_number ], string, required
+      def self.update_itn(shipment_id, params={})
+        response, api_key = ShipHawk.request(:put, "/shipments/#{shipment_id}", api_key, params)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
-      def shipments_bol_pdf shipment_id
-        get_request shipments_path("#{shipment_id}/bol.pdf"), {}
+      # get a shipment's BOL pdf url
+      def self.get_bol_url(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/bol", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
-      def shipments_create options
-        post_request shipments_path, options
+      # Return a shipment's BOL pdf binary
+      def self.get_bol_pdf(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/bol.pdf", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
-      def shipments_update shipment_id, options
-        put_request shipments_path(shipment_id), options
+      # get tracking info for a specific shipment
+      def self.get_tracking(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/tracking", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
-      def shipments_destroy shipment_id
-        delete_request shipments_path(shipment_id), {}
+      # get a shipment's address labels url
+      def self.get_address_labels(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/address_labels", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # get all dispatches associated with a shipment
+      def self.get_dispatches(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/dispatches", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # dispatch a shipment
+      def self.dispatch(shipment_id)
+        response, api_key = ShipHawk.request(:post, "/shipments/#{shipment_id}/dispatches", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # retrieve the documents on a shipment
+      def self.get_documents(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/documents", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # update document type on the shipment
+      # @params [ document_id ], integer, required
+      #         [ document_type ], string, required
+
+      def self.update_documents(shipment_id, params={})
+        response, api_key = ShipHawk.request(:put, "/shipments/#{shipment_id}/documents", api_key, params)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # retrieve the customer notes for a shipment
+      def self.get_notes(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/notes", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # create a customer note for a shipment
+      def self.create_notes(shipment_id, params={})
+        response, api_key = ShipHawk.request(:post, "/shipments/#{shipment_id}/notes", api_key, params)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # retrieve a shipment's packages label URLs
+      def self.get_label_urls(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/packages/labels", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # retrieve a shipment's packages labels combined PDF URL
+      def self.get_labels_pdf(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/packages/labels/combined", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # return a shipment's Commercial Invoice pdf url
+      def self.get_commercial_invoice_url(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/commercial_invoice", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # return a shipment's Commercial Invoice pdf binary
+      def self.get_commercial_invoice_pdf(shipment_id)
+        response, api_key = ShipHawk.request(:get, "/shipments/#{shipment_id}/commercial_invoice.pdf", api_key)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # update tracking number for a shipment
+      # @params [ tracking_number ], string, required
+      def self.update_tracking_number(shipment_id, params={})
+        response, api_key = ShipHawk.request(:put, "/shipments/#{shipment_id}/tracking_number", api_key, params)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
+      end
+
+      # reconcile a shipments shipping cost
+      # @params [ id ], integer, required
+      #         [ carrier ], string, required
+      #         [ sum_of_costs ], number, required
+
+      def self.reconcile(shipment_id, params={})
+        response, api_key = ShipHawk.request(:post, "/shipments/#{shipment_id}/reconcile/shipping_cost", @api_key, params)
+        ShipHawk::Util::convert_to_ShipHawk_object(response, api_key) if response
       end
 
     end
